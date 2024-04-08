d869771c1e27b28c8cc9bbf54b4cedfb2f068c0e
Antti Kivimäki <antti.kivimaki@futurice.com>
2024-04-08 12:59:55 +0300

    feat: add project limitations controls to notification rules

--- notification.go
+++ notification.go
@@ -35,84 +35,104 @@ type NotificationService struct {
 	client *Client
 }
 
-func (ps NotificationService) GetAllPublishers(ctx context.Context) (p []NotificationPublisher, err error) {
-	req, err := ps.client.newRequest(ctx, http.MethodGet, "/api/v1/notification/publisher")
+func (ns NotificationService) GetAllPublishers(ctx context.Context) (p []NotificationPublisher, err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodGet, "/api/v1/notification/publisher")
 	if err != nil {
 		return
 	}
 
-	_, err = ps.client.doRequest(req, &p)
+	_, err = ns.client.doRequest(req, &p)
 	return
 }
 
-func (ps NotificationService) CreatePublisher(ctx context.Context, publisher NotificationPublisher) (r NotificationPublisher, err error) {
-	req, err := ps.client.newRequest(ctx, http.MethodPut, "/api/v1/notification/publisher", withBody(publisher))
+func (ns NotificationService) CreatePublisher(ctx context.Context, publisher NotificationPublisher) (r NotificationPublisher, err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodPut, "/api/v1/notification/publisher", withBody(publisher))
 	if err != nil {
 		return
 	}
 
-	_, err = ps.client.doRequest(req, &r)
+	_, err = ns.client.doRequest(req, &r)
 	return
 }
 
-func (ps NotificationService) UpdatePublisher(ctx context.Context, publisher NotificationPublisher) (r NotificationPublisher, err error) {
-	req, err := ps.client.newRequest(ctx, http.MethodPost, "/api/v1/notification/publisher", withBody(publisher))
+func (ns NotificationService) UpdatePublisher(ctx context.Context, publisher NotificationPublisher) (r NotificationPublisher, err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodPost, "/api/v1/notification/publisher", withBody(publisher))
 	if err != nil {
 		return
 	}
 
-	_, err = ps.client.doRequest(req, &r)
+	_, err = ns.client.doRequest(req, &r)
 	return
 }
 
-func (ps NotificationService) DeletePublisher(ctx context.Context, ruleUuid uuid.UUID) (err error) {
-	req, err := ps.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/notification/publisher/%s", ruleUuid.String()))
+func (ns NotificationService) DeletePublisher(ctx context.Context, ruleUuid uuid.UUID) (err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/notification/publisher/%s", ruleUuid.String()))
 	if err != nil {
 		return
 	}
 
-	_, err = ps.client.doRequest(req, nil)
+	_, err = ns.client.doRequest(req, nil)
 	return
 }
 
-func (ps NotificationService) GetAllRules(ctx context.Context) (r []NotificationRule, err error) {
-	req, err := ps.client.newRequest(ctx, http.MethodGet, "/api/v1/notification/rule")
+func (ns NotificationService) GetAllRules(ctx context.Context) (r []NotificationRule, err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodGet, "/api/v1/notification/rule")
 	if err != nil {
 		return
 	}
 
-	_, err = ps.client.doRequest(req, &r)
+	_, err = ns.client.doRequest(req, &r)
 	return
 }
 
-func (ps NotificationService) CreateRule(ctx context.Context, rule NotificationRule) (r NotificationRule, err error) {
-	req, err := ps.client.newRequest(ctx, http.MethodPut, "/api/v1/notification/rule", withBody(rule))
+func (ns NotificationService) CreateRule(ctx context.Context, rule NotificationRule) (r NotificationRule, err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodPut, "/api/v1/notification/rule", withBody(rule))
 	if err != nil {
 		return
 	}
 
-	_, err = ps.client.doRequest(req, &r)
+	_, err = ns.client.doRequest(req, &r)
 	return
 }
 
-func (ps NotificationService) UpdateRule(ctx context.Context, rule NotificationRule) (r NotificationRule, err error) {
-	req, err := ps.client.newRequest(ctx, http.MethodPost, "/api/v1/notification/rule", withBody(rule))
+func (ns NotificationService) UpdateRule(ctx context.Context, rule NotificationRule) (r NotificationRule, err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodPost, "/api/v1/notification/rule", withBody(rule))
 	if err != nil {
 		return
 	}
 
-	_, err = ps.client.doRequest(req, &r)
+	_, err = ns.client.doRequest(req, &r)
 	return
 }
 
-func (ps NotificationService) DeleteRule(ctx context.Context, ruleUuid uuid.UUID) (err error) {
-	req, err := ps.client.newRequest(ctx, http.MethodDelete, "/api/v1/notification/rule", withBody(struct {
+func (ns NotificationService) DeleteRule(ctx context.Context, ruleUuid uuid.UUID) (err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodDelete, "/api/v1/notification/rule", withBody(struct {
 		UUID uuid.UUID `json:"uuid"`
 	}{UUID: ruleUuid}))
 	if err != nil {
 		return
 	}
 
-	_, err = ps.client.doRequest(req, nil)
+	_, err = ns.client.doRequest(req, nil)
+	return
+}
+
+func (ns NotificationService) AddProjectToRule(ctx context.Context, ruleUuid, projectUuid uuid.UUID) (r NotificationRule, err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodPost, fmt.Sprintf("/api/v1/notification/rule/%s/project/%s", ruleUuid.String(), projectUuid.String()))
+	if err != nil {
+		return
+	}
+
+	_, err = ns.client.doRequest(req, &r)
+	return
+}
+
+func (ns NotificationService) DeleteProjectFromRule(ctx context.Context, ruleUuid, projectUuid uuid.UUID) (r NotificationRule, err error) {
+	req, err := ns.client.newRequest(ctx, http.MethodDelete, fmt.Sprintf("/api/v1/notification/rule/%s/project/%s", ruleUuid.String(), projectUuid.String()))
+	if err != nil {
+		return
+	}
+
+	_, err = ns.client.doRequest(req, &r)
 	return
 }
